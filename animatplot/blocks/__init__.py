ce80eabf9dad78da4222736654dfad8d6ca379ed
Tyler Makaro <tyler@makaro.com>
2018-08-04 17:56:49 -0700

    fix blocks __init__

--- animatplot/blocks/__init__.py
+++ animatplot/blocks/__init__.py
@@ -1,5 +1,5 @@
 from .base import Block
 from .lineplots import Line, ParametricLine
 from .vectors import Quiver
-from .image_like import pcolormesh, imshow
+from .image_like import Pcolormesh, Imshow
 from .nuke import Nuke
