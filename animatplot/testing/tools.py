c13c93d13a0aca0522cb98d622dcc6f5fc74b278
Tyler Makaro <tyler@makaro.com>
2018-08-10 18:38:04 -0700

    added a decorator

--- animatplot/testing/tools.py
+++ animatplot/testing/tools.py
@@ -1,4 +1,9 @@
 import os
+from os.path import join, split
+import functools
+import pytest
+
+import matplotlib.pyplot as plt
 from matplotlib.animation import FileMovieWriter
 from matplotlib.testing.compare import compare_images
 from matplotlib.testing.exceptions import ImageComparisonFailure
@@ -40,16 +45,20 @@ class BunchOFiles(FileMovieWriter):
         self._frame_sink().close()
 
 
-def compare_animation(anim, expected, nframes, tol):
-    __tracebackhide__ = True
+def compare_animation(anim, expected, format_, nframes, tol):
     # generate images from the animation
-    anim.save('tests/output_images/' + expected, writer=BunchOFiles())
+    base_dir, filename = split(join('tests', 'baseline_images', expected))
+    out_dir = split(join('tests', 'output_images', expected))[0]
 
-    name, format = expected.split('.')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    anim.save(os.path.join(out_dir, (filename+format_)), writer=BunchOFiles())
+
     for i in range(nframes):
-        image_name = '%s%d.%s' % (name, i, format)
-        expected_name = 'tests/baseline_images/' + image_name
-        actual_name = 'tests/output_images/' + image_name
+        image_name = '%s%d%s' % (filename, i, format_)
+        expected_name = os.path.join(base_dir, image_name)
+        actual_name = os.path.join(out_dir, image_name)
 
         err = compare_images(expected_name, actual_name, tol,
                              in_decorator=True)
@@ -62,4 +71,16 @@ def compare_animation(anim, expected, nframes, tol):
             for key in ["actual", "expected"]:
                 err[key] = os.path.relpath(err[key])
             raise ImageComparisonFailure(
-                'images not close (RMS %(rms).3f):\n\t%(actual)s\n\t%(expected)s ' % err)
+                'images not close (RMS %(rms).3f):\n\t%(actual)s\n'
+                '\t%(expected)s ' % err)
+
+
+def animation_compare(baseline_images, nframes, format='.png', tol=1e-3):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            anim = func(*args, **kwargs)
+            compare_animation(anim, baseline_images, format, nframes, tol)
+            plt.close('all')
+        return wrapper
+    return decorator
--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -2,9 +2,10 @@ import matplotlib
 matplotlib.use('agg')
 import numpy as np
 import animatplot as amp
-from animatplot.testing.comparison import compare_animation, BunchOFiles
+from animatplot.testing import animation_compare
 
 
+@animation_compare(baseline_images='Blocks/Line', nframes=5)
 def test_Line():
     x = np.linspace(0, 2*np.pi, 20)
     t = np.linspace(0, 2*np.pi, 5)
@@ -12,6 +13,16 @@ def test_Line():
     X, T = np.meshgrid(x, t)
     Y = np.sin(X+T)
     block = amp.blocks.Line(X, Y)
-    anim = amp.Animation([block])
+    return amp.Animation([block])
 
-    compare_animation(anim, 'Line.png', 5, 1e-3)
+
+@animation_compare(baseline_images='Blocks/Pcolormesh', nframes=3)
+def test_Pcolormesh():
+    x = np.linspace(-2*np.pi, 2*np.pi, 100)
+    t = np.linspace(0, 2*np.pi, 3)
+
+    X, Y, T = np.meshgrid(x, x, t)
+    Z = np.sin(X**2+Y**2-T)
+
+    block = amp.blocks.Pcolormesh(X[:, :, 0], Y[:, :, 0], Z, t_axis=2)
+    return amp.Animation([block])
