20ad0c4f4f741a418924c812d3e76167bb46d69f
Thomas Nicholas <tegn500@york.ac.uk>
2019-01-09 19:56:08 +0000

    Organised tests into classes

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -47,83 +47,83 @@ class TestTitleBlock:
         actual = Title('timestep {values:.2f}', values=[5e7]).titles
         assert actual == ['timestep 50000000.00']
 
+    def test_format_str_numpy_arrays(self):
+        actual = Title('timestep {num}', num=np.array([1, 2])).titles
+        assert actual == ['timestep 1', 'timestep 2']
+
     # Hypothesis test that the strings are always formatted correctly?
 
-    @pytest.mark.skip
     def test_text(self):
         # TODO test that the right type of object is produced?
-        actual = Title('timestep {num}', num=[1, 2]).text
+        title_block = Title('timestep {num}', num=[1, 2])
 
-    @pytest.mark.skip
-    def test_update(self):
-        ...
+        ax = plt.gca()
+        assert ax.get_title() == 'timestep 1'
+        title_block._update(1)
+        assert ax.get_title() == 'timestep 2'
+        plt.close('all')
 
-    @pytest.mark.skip
     def test_mpl_kwargs(self):
         expected = {'loc': 'left', 'fontstyle': 'italic'}
         actual = Title('timestep {num}', num=[1, 2], mpl_kwargs=expected)
         assert actual._mpl_kwargs == expected
 
 
-@animation_compare(baseline_images='Blocks/Line', nframes=5)
-def test_Line():
-    x = np.linspace(0, 2*np.pi, 20)
-    t = np.linspace(0, 2*np.pi, 5)
+class TestComparisons:
+    @animation_compare(baseline_images='Blocks/Line', nframes=5)
+    def test_Line(self):
+        x = np.linspace(0, 2*np.pi, 20)
+        t = np.linspace(0, 2*np.pi, 5)
 
-    X, T = np.meshgrid(x, t)
-    Y = np.sin(X+T)
-    block = amp.blocks.Line(X, Y)
-    return amp.Animation([block])
+        X, T = np.meshgrid(x, t)
+        Y = np.sin(X+T)
+        block = amp.blocks.Line(X, Y)
+        return amp.Animation([block])
 
+    @animation_compare(baseline_images='Blocks/Pcolormesh', nframes=3)
+    def test_Pcolormesh(self):
+        x = np.linspace(-2*np.pi, 2*np.pi, 100)
+        t = np.linspace(0, 2*np.pi, 3)
 
-@animation_compare(baseline_images='Blocks/Pcolormesh', nframes=3)
-def test_Pcolormesh():
-    x = np.linspace(-2*np.pi, 2*np.pi, 100)
-    t = np.linspace(0, 2*np.pi, 3)
+        X, Y, T = np.meshgrid(x, x, t)
+        Z = np.sin(X**2+Y**2-T)
 
-    X, Y, T = np.meshgrid(x, x, t)
-    Z = np.sin(X**2+Y**2-T)
+        block = amp.blocks.Pcolormesh(X[:, :, 0], Y[:, :, 0], Z, t_axis=2)
+        return amp.Animation([block])
 
-    block = amp.blocks.Pcolormesh(X[:, :, 0], Y[:, :, 0], Z, t_axis=2)
-    return amp.Animation([block])
+    @animation_compare(baseline_images='Blocks/Imshow', nframes=3)
+    def test_Imshow(self):
+        x = np.linspace(0, 1, 10)
+        X, Y = np.meshgrid(x, x)
 
+        U = []
+        for i in range(3):
+            U.append(X**2+Y**2+i)
 
-@animation_compare(baseline_images='Blocks/Imshow', nframes=3)
-def test_Imshow():
-    x = np.linspace(0, 1, 10)
-    X, Y = np.meshgrid(x, x)
+        block = amp.blocks.Imshow(U)
+        return amp.Animation([block])
 
-    U = []
-    for i in range(3):
-        U.append(X**2+Y**2+i)
+    @animation_compare(baseline_images='Blocks/Quiver', nframes=4)
+    def test_Quiver(self):
+        x = np.linspace(0, 1, 10)
+        X, Y = np.meshgrid(x, x)
 
-    block = amp.blocks.Imshow(U)
-    return amp.Animation([block])
+        U, V = [], []
+        for i in range(4):
+            U.append(X**2+Y**2+i)
+            V.append(X**2+Y**2+i)
 
+        block = amp.blocks.Quiver(X, Y, U, V)
+        return amp.Animation([block])
 
-@animation_compare(baseline_images='Blocks/Quiver', nframes=4)
-def test_Quiver():
-    x = np.linspace(0, 1, 10)
-    X, Y = np.meshgrid(x, x)
+    @animation_compare(baseline_images='Blocks/Nuke', nframes=3)
+    def test_Nuke(self):
+        ax = plt.gca()
+        sizes = []
 
-    U, V = [], []
-    for i in range(4):
-        U.append(X**2+Y**2+i)
-        V.append(X**2+Y**2+i)
-
-    block = amp.blocks.Quiver(X, Y, U, V)
-    return amp.Animation([block])
-
-
-@animation_compare(baseline_images='Blocks/Nuke', nframes=3)
-def test_Nuke():
-    ax = plt.gca()
-    sizes = []
-
-    def animate(i):
-        sizes.append(i+1)
-        ax.set_aspect("equal")
-        ax.pie(sizes)
-    block = amp.blocks.Nuke(animate, length=3, ax=ax)
-    return amp.Animation([block])
-
+        def animate(i):
+            sizes.append(i+1)
+            ax.set_aspect("equal")
+            ax.pie(sizes)
+        block = amp.blocks.Nuke(animate, length=3, ax=ax)
+        return amp.Animation([block])
